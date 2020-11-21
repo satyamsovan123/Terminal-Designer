1b17ae8134268b18c050df85e2e146e7548c1012
Satyam Sovan Mishra <msatyamsovan@gmail.com>
2020-11-21 20:20:13 +0530

    fixed main
    fixed issue where \n is not considered as an escape sequence

--- Terminal Designer/main.sh
+++ Terminal Designer/main.sh
@@ -2,6 +2,10 @@
 clear
 
 echo "Enter \e[0;36mpassword\e[0m"
-sudo echo '\n#Added by Terminal Designer\n#!/bin/zsh\ncd ~/Terminal\ Designer\nchmod a+x change-logo.sh\nsh change-logo.sh' >> ~/.zshrc
+sudo echo '#Added by Terminal Designer' >> ~/.zshrc
+sudo echo '#!/bin/zsh'>> ~/.zshrc
+sudo echo 'cd ~/Terminal\ Designer' >> ~/.zshrc
+sudo echo 'chmod a+x change-logo.sh\nsh change-logo.sh' >> ~/.zshrc
+sudo echo 'sh change-logo.sh' >> ~/.zshrc
 echo "\e[0;33mRestart the terminal\e[0m to enjoy \e[1;31mASCII Arts!\e[0m"
 echo "Visit \e[0;34mhttps://www.asciiart.eu\e[0m for more \e[1;31mARTS\e[0m"
